d9862bf4cd144fd1ef32089492655b530dda52ea
Kamalashree <kamalashree.jd@googlemail.com>
2026-09-16 17:46:46 +0100

    Add files via upload
    Code for the total_expenses function has been written. Doc strings have been edited to match the conventions.

--- expense_tracker.py
+++ expense_tracker.py
@@ -13,8 +13,8 @@ def show_menu():
 
 
 def add_category(categories):
-    """Function asks fo a new category name, checks that it does not already exist and it appends the new category to a list of categories. 
-    The program must make the categories case insensitive and ignore any spaces at the start or end of the string in order to avoid replicas.
+    """Ask for a new category name, check that it does not already exist and append the new category to a list of categories. 
+    Make the categories case insensitive and ignore any spaces at the start or end of the string in order to avoid replicas.
     """
     new_category = input("Enter the name of the new category: ").strip()
 
@@ -32,11 +32,10 @@ def add_category(categories):
 
 
 def view_categories(categories):
-    """The function checks if the list of categories is empty or not.
-     If it is empty, it says that there are no existing categories 
-     and the return statement stops the program from running the code that returns the 
+    """Check if the list of categories is empty or not.
+     If it is empty, say that there are no existing categories 
+     and stop the return statement  program from running the code that returns the 
      categories in a numbered format as there are no categories.
-     The enumerate function assigns an index to each category in the list starting with 1 and prints the numbered categories.
      """
     
     if len(categories) == 0: 
@@ -44,16 +43,17 @@ def view_categories(categories):
         return 
 
     print("\n----Categories----")
-    for i, category in enumerate(categories,start=1):
+    for i, category in enumerate(categories,start=1): #The enumerate function assigns an index to each 
+        #category in the list starting with 1 and prints the numbered categories.
         print(f"{i}. {category}")
 
 
 def delete_category(categories, expenses):
-    """If categories is empty, function prints a message stating that.
-    Categories are displayed using view_categories() so that user can see what they want to delete.
-    Program asks which category number to delete.
-    If there are any expenses in that category, program asks user to confirm whether they want the category to be deleted or not
-    If there are no expenses in that category, category is deleted with no confirmation needed from user."""
+    """If categories is empty, print a message stating that.
+    Display categories using view_categories() so that user can see what they want to delete.
+    Ask which category number to delete.
+    If there are any expenses in that category, ask user to confirm whether they want the category to be deleted or not
+    If there are no expenses in that category, delete with no confirmation needed from user."""
 
     if len(categories) == 0: #checking if any categories exist or not 
         print("No existing categories to delete")
@@ -83,13 +83,14 @@ def delete_category(categories, expenses):
             print("Deletion cancelled")
             return
 
-        expenses[:] = [e for e in expenses if e["category"] != category_to_delete] #edits list of expenses by including all the expenses except the one in the category to be deleted.
+        expenses[:] = [e for e in expenses if e["category"] != category_to_delete] 
+        #edits list of expenses by including all the expenses except the one in the category to be deleted.
         categories.remove(category_to_delete)
         print(f"Deleted category: {category_to_delete}")
 
 
 def add_expense(expenses, categories):
-    """If category is empty, then program tells user to add a category first.
+    """If category is empty, tells user to add a category first.
     Show all the categories and ask user to choose a category number to add expense to.
     Make sure that category number exists.
     Ask for an expense and append it to expense list as a dictionary of category and amount
@@ -107,8 +108,6 @@ def add_expense(expenses, categories):
     except (ValueError, IndexError):
         print("Invalid category choice.")
         return
-
-    view_categories(categories)
 
     try:
         amount = float(input("Entwr expense amount: "))
@@ -130,21 +129,50 @@ def view_expenses(expenses):
     print(f"\n{'Category': <15}{'Amount':>10}") #category and amount are columns and expenses are listed as rows of category and amount.
     print("-" * 25)
     for e in expenses:
-        print(f"{e['category']: <15}{e['amount']:>10.2f}") #rows are alignded properly under the column headings and expenses are rounded to 2 decimal places.
+        print(f"{e['category']: <15}{e['amount']:>10.2f}") #rows are alignded properly under the 
+        #column headings and expenses are rounded to 2 decimal places.
 
 
 def delete_expense(expenses):
-    """Expenses are displayed with indexing and grouped into categories.
-    Program asks which expense number to delete and deletes it after making sure the expense exists."""
+    """Display expenses with indexing and grouped into categories.
+    Ask which expense number to delete and delete it after making sure the expense exists.
+    """
     pass
 
 
 def total_expenses(expenses):
     """If expenses list is empty, print a message stating that.
     Loop through expenses (list of dictionaries) to fill a new dictionary of category totals.
-    The grand total of expenses is displayed.
+    Display the grand total of expenses.
+
+    The category_totals is a dictionary where each key is a category and the value assigned to 
+    that key is the total expense for that category.
+
     """
-    pass
+    if len(expenses) == 0:
+        print("No expenses added yet.")
+        return
+
+    category_totals = {}
+    grand_total = 0
+
+    for e in expenses:
+        category_totals[e['category']] = category_totals.get(e['category'], 0) + e['amount'] 
+        #adds the expense amount in the currect dictionary being looked at in the expenses list
+        #to the value at that category key in the category_totals dictionary.
+        #if a key with that category name does not exist, a new key for that category is created in category_totals
+        # and the expense amount is assigned to it.   
+        grand_total += e['amount'] #total expense
+
+    print(f"\n{'Category':<15}{'Total':>10}") #creates two columns with headings 'category' and 'total'.
+    print("-" * 25)
+
+    for category, total in category_totals.items():
+        print(f"{category:<15}{total:>10.2f}") #each category and its respective total expense is printed in rows. 
+
+    print("-" * 25)
+    print(f"{'Grand Total':<15}{grand_total:>10.2f}")
+
 
 
 def load_expenses(filename):
